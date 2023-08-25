bfdaf01a8f9416eeb3bd28f5ed8b7cccae24cde1
vikashyav <142897671+vikashyav@users.noreply.github.com>
2023-08-25 08:11:47 +0000

    improve movie thumbnail selection

--- src/components/movie-collection/thumbnail-selecton.js
+++ src/components/movie-collection/thumbnail-selecton.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import Skeleton from "../skeleton";
 
-export default function ThumbnailSelecton() {
+function Selecton() {
+    return (
+        <>
+            <div className="min-h-[260px] flex flex-col bg-[#282c34] text-[white] cursor-pointer m-[5px] p-[5px] rounded-[10px] hover:bg-[white] hover:text-[black]">
+                <div className="flex items-center mt-auto">
+                    <Skeleton className="w-6 rounded-full h-6 mr-1 mt-1" variant="circular" />
+                    <Skeleton className="w-7/12 mx-2 h-4 rounded-sm" variant="circular" />
+                </div>
+                <div className="flex pl-7">
+                    <Skeleton className="w-5/12 mx-2 h-3 rounded-sm" variant="circular" />
+                </div>
+            </div>
+        </>
+    )
+  }
+  
+export default function ThumbnailSelectons() {
   return (
       <>
-          <div className="flex flex-col bg-[#282c34] text-[white] cursor-pointer m-[5px] p-[5px] rounded-[10px] hover:bg-[white] hover:text-[black]">
-              <div className="flex items-center mt-auto">
-                  <Skeleton className="w-6 rounded-full h-6 mr-1 mt-1" variant="circular" />
-                  <Skeleton className="w-7/12 mx-2 h-4 rounded-sm" variant="circular" />
-              </div>
-              <div className="flex pl-7">
-                  <Skeleton className="w-5/12 mx-2 h-3 rounded-sm" variant="circular" />
-              </div>
-          </div>
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
+        <Selecton />
       </>
   )
 }
