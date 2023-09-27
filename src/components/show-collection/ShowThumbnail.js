bc8ae8a0730695b050c219ac822ea6cb34f814e9
vikas kumar <vikaskky@gmail.com>
2023-09-27 10:29:52 +0530

    fix mirror issue in series page

--- src/components/show-collection/ShowThumbnail.js
+++ src/components/show-collection/ShowThumbnail.js
@@ -16,9 +16,9 @@ function ShowThumbnail({ result, isSeriesSeason, series_id, series_name }) {
     }
   };
 
-  let slugifyUrl = `/${checkTvOrMovieFromTitle(result.original_title, result.name)}/${slugify(series_name|| result?.name || (result?.original_title || result?.original_name))}-${series_id || result.id}`;
- 
-  if (isSeriesSeason ==="Seasons") {
+  let slugifyUrl = `/${checkTvOrMovieFromTitle(result.original_title, result.name)}/${slugify(series_name || result?.name || (result?.original_title || result?.original_name))}-${series_id || result.id}`;
+
+  if (isSeriesSeason === "Seasons") {
     slugifyUrl = `${slugifyUrl}?season_number=${result?.season_number}`
   }
 
@@ -30,33 +30,31 @@ function ShowThumbnail({ result, isSeriesSeason, series_id, series_name }) {
                rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px]
         border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl
          transform hover:scale-105 transition duration-300`}
-        // w-[210px] smb:w-[150px] smb:h-[250px] sm:w-[190px] sm:h-[300px]
-        // onClick={() => router.push(slugifyUrl)}
-        >
-      <img
+      // w-[210px] smb:w-[150px] smb:h-[250px] sm:w-[190px] sm:h-[300px]
+      // onClick={() => router.push(slugifyUrl)}
+      >
+        <img
           src={THUMBNAIL_URL}
-          onError={({ currentTarget }) => {
-            currentTarget.onerror = null; // prevents looping
-            currentTarget.src = "/images/nothing.svg";
-          }}
-        // width={190}
-        // height={330}
           alt={`${result.name}, ${constant.ATTRIBUTES.IMG}`}
           title={`${result.name}, ${constant.ATTRIBUTES.IMG}`}
-            // objectFit="cover"
-            layout="fill"
+          layout="fill"
           // className="rounded-lg  aspect-[4/3]"
           className="my-auto w-full min-h-[260px] rounded-[7px] hover:scale-105"
-          />
-          <div
-            className="absolute m-0 px-2 py-1 bottom-0  md:inset-y-auto md:bottom-0 backdrop-blur-sm bg-black/30 inset-x-0 md:inset-x-0 space-y-4 z-50"
+          // onError={({ currentTarget }) => {
+          //   currentTarget.onerror = null; // prevents looping
+          //   currentTarget.src = "/images/nothing.svg";
+          // }}
+          onerror="this.src='/images/nothing.svg';"
+        />
+        <div
+          className="absolute m-0 px-2 py-1 bottom-0  md:inset-y-auto md:bottom-0 backdrop-blur-sm bg-black/30 inset-x-0 md:inset-x-0 space-y-4 z-50"
 
-          >
-            <h2 className="font-bold">
-              {(result.title || result.original_name )|| `${result.name}. ${ result.episode_count} Episodes` }
-              </h2>
-          </div>
+        >
+          <h2 className="font-bold">
+            {(result.title || result.original_name) || `${result.name}. ${result.episode_count} Episodes`}
+          </h2>
         </div>
+      </div>
     </Link>
   );
 }
